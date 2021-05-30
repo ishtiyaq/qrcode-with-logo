b0602eaee3159148351b98f4e8d72bf49bd4e162
Ishtiyaq Husain <ishtiyaq.husain@gmail.com>
2021-05-30 14:43:42 +0530

    fix variable name

--- qr_code.php
+++ qr_code.php
@@ -13,24 +13,24 @@ header('Content-type: image/png');
 
 // Get QR Code image from Google Chart API
 // http://code.google.com/apis/chart/infographics/docs/qr_codes.html
-$QR = imagecreatefrompng('https://chart.googleapis.com/chart?cht=qr&chld=H|1&chs='.$size.'&chl='.urlencode($data));
+$qr = imagecreatefrompng('https://chart.googleapis.com/chart?cht=qr&chld=H|1&chs='.$size.'&chl='.urlencode($data));
 
 if($logo !== FALSE) {
 	$logo = imagecreatefromstring(file_get_contents($logo));
 
-	$QR_width = imagesx($QR);
-	$QR_height = imagesy($QR);
+	$qr_width = imagesx($qr);
+	$qr_height = imagesy($qr);
 	
 	$logo_width = imagesx($logo);
 	$logo_height = imagesy($logo);
 	
 	// Scale logo to fit in the QR Code
-	$logo_qr_width = $QR_width/3;
+	$logo_qr_width = $qr_width/3;
 	$scale = $logo_width/$logo_qr_width;
 	$logo_qr_height = $logo_height/$scale;
 	
-	imagecopyresampled($QR, $logo, $QR_width/3, $QR_height/3, 0, 0, $logo_qr_width, $logo_qr_height, $logo_width, $logo_height);
+	imagecopyresampled($qr, $logo, $qr_width/3, $qr_height/3, 0, 0, $logo_qr_width, $logo_qr_height, $logo_width, $logo_height);
 }
 
-imagepng($QR);
-imagedestroy($QR);
+imagepng($qr);
+imagedestroy($qr);
